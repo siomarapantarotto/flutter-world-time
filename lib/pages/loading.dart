8c45f9e5fe56811568a97611485fd5c55cb9b738
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2021-11-09 02:08:27 -0300

    Implement jsonDecode from dart:convert

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -12,14 +13,21 @@ class Loading extends StatefulWidget {
 class _LoadingState extends State<Loading> {
 
   void getData() async {
-    //var url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
+    var url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
     //var url = Uri.parse('http://localhost:9060/v1/funcoes');
-    var url = Uri.parse('http://10.8.41.152:9060/v1/funcoes');
+    //var url = Uri.parse('http://10.8.41.152:9060/v1/funcoes');
     //var response = await http.post(url, body: {'name': 'doodle', 'color': 'blue'});
     Response response = await http.get(url);
-    //print('Response status: ${response.statusCode}');
-    //print('Response body: ${response.body}');
-    print(response.body);
+    print('Response status: ${response.statusCode}');
+    // body looks like a map but it is a string
+    print('Response body: ${response.body}');
+    // here is the way to decode the string to extract the attributes
+    Map data = jsonDecode(response.body);
+    print(data);
+    print(data['userId']);
+    print(data['id']);
+    print(data['title']);
+    print(data['completed']);
 
   }
 
