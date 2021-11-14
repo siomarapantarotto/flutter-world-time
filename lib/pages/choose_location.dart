7c16eb4d17374fe3f8d140b81c239e8c4ad071dc
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2021-11-14 13:27:11 -0300

    Add image assets

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
   const ChooseLocation({Key? key}) : super(key: key);
@@ -8,6 +10,36 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Berlin', flag: 'germany.png'),
+    WorldTime(url: 'Europe/Athens', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Europe/Lisbon', location: 'Lisbon', flag: 'portugal.png'),
+    WorldTime(url: 'Europe/Madrid', location: 'Madrid', flag: 'spain.png'),
+    WorldTime(url: 'Europe/Rome', location: 'Rome', flag: 'italy.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'America/Sao_Paulo', location: 'Brasilia', flag: 'brazil.png'),
+    WorldTime(url: 'America/Sao_Paulo', location: 'Sao Paulo', flag: 'brazil.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
+
+  void updateTime(index) async {
+    // Not creating an instance, just storing WorldTime instance inside local variable.
+    WorldTime wtInstance = locations[index];
+    await wtInstance.getTime();
+    // Navigate to home screen.
+    Navigator.pop(context, {
+      'location': wtInstance.location,
+      'flag': wtInstance.flag,
+      'time': wtInstance.time,
+      'isDayTime': wtInstance.isDayTime
+    });
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,17 +48,30 @@ class _ChooseLocationState extends State<ChooseLocation> {
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
         backgroundColor: Colors.blue[900],
-        title: Text('Choose a Location'),
+        title: const Text('Choose a Location'),
         centerTitle: true,
         elevation: 0,
       ),
-      // body: ElevatedButton(
-      //   onPressed: () {
-      //     setState(() {
-      //       counter += 1;
-      //     });
-      //   },
-      //   child: Text('counter is $counter'),),
+      body: ListView.builder(
+          itemCount: locations.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+              child: Card(
+                  child: ListTile(
+                onTap: () {
+                  //print(locations[index].location);
+                  updateTime(index);
+                },
+                title: Text(locations[index].location),
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage(
+                    'assets/${locations[index].flag}',
+                  ),
+                ),
+              )),
+            );
+          }),
     );
   }
 }
